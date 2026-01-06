46aced9a6f782e895c0cdbf91fd487d32a0baecf
Maiku <maiku@private.local>
2026-01-06 09:37:54 +0900

    Site update: 2026/01/06  9:37:54.55

--- announcements.js
+++ announcements.js
@@ -1,4 +1,9 @@
 ﻿const announcements = [
+    {
+        title: "試験的にこのサイトを公開・運用を始めます！！",
+        url: "news_20260106_0935.html",
+        date: "26/01/06/09:35"
+    },
 ];
 
 // Added by update-blog-index.ps1
